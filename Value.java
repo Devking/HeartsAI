0ab15be925b63cbccf10c03a837d0c1d09d81f2b
Wells Santo <wls254@nyu.edu>
2015-05-04 23:05:14 -0400

    added some comments

--- Value.java
+++ Value.java
@@ -1,2 +1,16 @@
 // note that enums naturally implement Comparable! They are comparable in the order given below
 public enum Value { ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE }
+
+/*
+How to compare enums:
+
+Value one = Value.ONE;
+Value two = Value.TWO;
+
+// compareTo will be positive if the caller is larger than the parameter
+// otherwise it will be negative
+if (one.compareTo(two) < 0) {
+	System.out.println("this will be printed because one is less than two");
+}
+
+*/
