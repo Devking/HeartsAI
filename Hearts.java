8422dec8df4a2ddbc7cd551044630936bb0ad70c
Wells Santo <wls254@nyu.edu>
2015-05-04 23:23:23 -0400

    more basic class additions

--- Hearts.java
+++ Hearts.java
@@ -1,13 +1,8 @@
-// Classes needed:
-// Card class (for each individual card)
-// Deck class (for the "deck" to deal hands)
-// Hand class (for each of the hands)
-// Game class (plays one game)
-
 public class Hearts {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Hearts version 0.0.1.");
 		Deck thing = new Deck();
+		Game round = new Game();
 		thing.printNumber();
 	}
 }
